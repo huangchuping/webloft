b1bbb86631ce7512a1cb627a602e1fc1fab5adc5
huangcp <hcp0224@qq.com>
2014-12-23 16:40:31 +0800

    '删除废文件'

--- public/captcha.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-require '';  //�Ȱ���������ʵ��·�����ʵ����������޸ġ�
-$_vc = new ValidateCode();		//ʵ��һ������
-$_vc->doimg();		
-$_SESSION['authnum_session'] = $_vc->getCode();//��֤�뱣�浽SESSION��
-?>
